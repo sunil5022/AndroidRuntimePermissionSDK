900ce96fb237987c6b951f56e007f081fd98e35a
Sunil K <sunilk.offsureit@gmail.com>
2024-09-24 15:09:44 +0530

    configured the gradle

--- runTimePermissionSDK/build.gradle.kts
+++ runTimePermissionSDK/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -29,6 +30,16 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+}
+
+publishing{
+    publications{
+        register<MavenPublication>("release"){
+            afterEvaluate{
+                from(components["release"])
+            }
+        }
     }
 }
 
